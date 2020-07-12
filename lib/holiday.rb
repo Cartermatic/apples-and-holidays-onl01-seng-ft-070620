8916e2ba2b0ab429d10ab06e07afc414b60076a3
Carter Parrett <cparrett94@hotmail.com>
2020-07-12 22:49:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,7 +43,9 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  
+  # expect(holiday_supplies[:fall].keys).not_to include(:columbus_day)
+
+  #     columbus_day_supplies = ["Flags", "Parade Floats", "Italian Food"]
 end
 
 def all_winter_holiday_supplies(holiday_hash)
