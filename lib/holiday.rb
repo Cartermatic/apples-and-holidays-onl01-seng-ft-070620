eb0f6ada62222d081330054ea3f6700fe04825b1
Carter Parrett <cparrett94@hotmail.com>
2020-07-13 02:25:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,9 @@ def all_supplies_in_holidays(holiday_hash)
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash
+  holiday_hash.each do |season, data|
+    data.each do |supply|
+      supply.each do 
 end
 
 
