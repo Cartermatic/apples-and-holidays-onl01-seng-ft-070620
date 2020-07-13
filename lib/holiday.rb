6390894d5f2440b74c0cb7752cfc846671b1e4a7
Carter Parrett <cparrett94@hotmail.com>
2020-07-13 03:08:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
  holiday_hash.each do |season, data|
-    puts "#{season.to_s.capitalize!}:"
+    puts "#{season.capitalize!}:"
     data.each do |holiday, supply|
       array = holiday.to_s.split("_")
       final_holiday = []
