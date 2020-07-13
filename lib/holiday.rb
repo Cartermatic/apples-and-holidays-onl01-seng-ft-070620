15b4080e0b3a55b8481f4356053c7a8ad444192f
Carter Parrett <cparrett94@hotmail.com>
2020-07-13 01:06:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,6 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash[:winter].collect = winter_supplies
-  winter_supplies
 
 end
 
